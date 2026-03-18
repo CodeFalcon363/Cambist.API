34a97d77483a9b3f2368bf34d5068443d1910091
user.name <anthonyoise@gmail.com>
2026-03-18 21:59:04 +0100

    added DI extension method and wired Infrastructure services

---
+++ Cambist.API/Extensions/ServiceExtensions.cs
@@ -0,0 +1,21 @@
+﻿using Cambist.Infrastructure.Interfaces;
+using Cambist.Infrastructure.Mappings;
+using Cambist.Infrastructure.Repositories;
+using Cambist.Infrastructure.ExternalServices;
+using AutoMapper;
+
+namespace Cambist.Api.Extensions
+{
+    public static class ServiceExtensions
+    {
+        public static IServiceCollection AddInfrastructure(this IServiceCollection services, IConfiguration configuration)    
+        {
+            services.AddAutoMapper(cfg => cfg.AddProfile<MappingProfile>());
+            services.AddScoped<IConversionRepository, ConversionRepository>();
+            services.AddScoped<IWatchlistRepository, WatchlistRepository>();
+            services.AddScoped<ICurrencyRepository, CurrencyRepository>();
+            services.AddScoped<ExchangeRateService>();
+            return services;
+        }
+    }
+}
--- Cambist.API/Program.cs
+++ Cambist.API/Program.cs
@@ -1,5 +1,6 @@
 using Microsoft.EntityFrameworkCore;
 using Cambist.Core.Data;
+using Cambist.Api.Extensions;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -10,6 +11,7 @@ builder.Services.AddControllers();
 builder.Services.AddOpenApi();
 builder.Services.AddDbContext<CambistDbContext>(options =>
     options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
+builder.Services.AddInfrastructure(builder.Configuration);
 
 var app = builder.Build();
 
