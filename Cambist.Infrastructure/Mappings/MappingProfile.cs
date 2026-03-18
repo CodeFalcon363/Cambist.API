3fc5baf4b9fc7c19b50c54eeda2ab1ca57fadd78
user.name <anthonyoise@gmail.com>
2026-03-18 19:32:22 +0100

    added AutoMapper profile with entity and model mappings

---
+++ Cambist.Infrastructure/Mappings/MappingProfile.cs
@@ -0,0 +1,20 @@
+﻿using AutoMapper;
+using Cambist.Core.Entities;
+using Cambist.Core.Models.Requests;
+using Cambist.Core.Models.Responses;
+
+namespace Cambist.Infrastructure.Mappings
+{
+    public class MappingProfile : Profile
+    {
+        public MappingProfile()
+        {
+            CreateMap<Currency, CurrencyResponse>().ReverseMap();
+            CreateMap<ConversionRecord, ConversionRecordResponse>().ReverseMap();
+            CreateMap<WatchlistItem, WatchlistItemResponse>().ReverseMap();
+            CreateMap<ConvertCurrencyRequest, ConversionRecord>();
+            CreateMap<AddWatchlistItemRequest, WatchlistItem>();
+
+        }
+    }
+}
